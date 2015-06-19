b856d1d75d8c60213c5e95f7eb67486ce891dfa9
unknown <abe12002@byui.edu>
2015-06-18 19:53:52 -0600

    Posts project

--- JSPDiscussionThread/src/main/java/com/mycompany/jspdiscussionthread/SignIn.java
+++ JSPDiscussionThread/src/main/java/com/mycompany/jspdiscussionthread/SignIn.java
@@ -5,14 +5,29 @@
  */
 package com.mycompany.jspdiscussionthread;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import static java.lang.System.out;
+import java.net.URL;
+import java.util.Properties;
+import java.util.Scanner;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import static javax.servlet.SessionTrackingMode.URL;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -33,11 +48,58 @@ public class SignIn extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-         out.println("hello");
-        }
-    
+        String username = request.getParameter("username");
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+        System.out.println(username);
+
+        //request.setAttribute("username", username);
+        //request.getRequestDispatcher("newPost.jsp").forward(request, response);
+        HttpSession session = request.getSession();
+        session.setAttribute("username", username);
+     
+        response.sendRedirect("newPost.jsp");
+ 
+//         = request.getSession();
+        //    String formUsername = request.getParameter("username");
+        //    String formPassword = request.getParameter("password");
+        //  String appPath = request.getServletContext().getRealPath("");
+        //   String filePath = appPath + File.separator + "login.txt";
+        //    ServletContext application = request.getServletContext();
+        //   InputStream inputStream = application.getResourceAsStream("/login.txt");
+     /*   
+         String path = getServletContext().getRealPath("/") + "myFile.txt";
+         // String path = "C:\\temp\\login.txt";
+        
+         System.out.println("path " + path);
+        
+         BufferedWriter out = new BufferedWriter(new FileWriter(path));
+        
+         out.write("test");
+         out.close();
+        
+        
+         BufferedReader in = new BufferedReader(new FileReader(path));
+        
+         String read = in.readLine();
+        
+         System.out.println(read);
+        
+         // BufferedReader fr = new BufferedReader(new FileReader("C:\\temp\\login.txt"));
+         */
+        /*
+         while (true) {
+         String line = fr.readLine();
+         if (line == null) {
+         break;
+         }
+         String[] words = line.split(" ");
+
+         System.out.println(words[0]);
+         System.out.println(words[1]);
+         }*/
+    }
+
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
